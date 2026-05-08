6fddb5f0a0f7a7855b4686929daac40a3fba1116
choidh88 <dev.cdh88@gmail.com>
2026-05-08 12:58:16 +0900

    Apply code formatter and use it.

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct playerType{
-
+struct playerType
+{
 };
 void print_board();
 bool valid_move();
 int won_seeds();
 void king_me();
 
-
 int main()
 {
-   printf("\t\t******************************************************\n");
-   printf("\t\t\tTHE CONSOLE VERSION OF THE CHECKERS GAME\n");
-   printf("\t\t******************************************************\n");
+    printf("\t\t******************************************************\n");
+    printf("\t\t\tTHE CONSOLE VERSION OF THE CHECKERS GAME\n");
+    printf("\t\t******************************************************\n");
     return 0;
 }
